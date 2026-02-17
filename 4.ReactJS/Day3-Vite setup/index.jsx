91c47f7022b8941f70fca7a75a560805967a0fae
Abhijeet singh <abhijeet.singh0513@gmail.com>
2026-02-17 23:32:03 +0530

    vite project

---
+++ 4.ReactJS/Day3-Vite setup/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const element = <h1>Hello vite</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
